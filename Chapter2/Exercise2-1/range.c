6c2cffcb8728fb891df7cf3606cad77d261061ba
Na Tan <nat1@andrew.cmu.edu>
2018-11-05 18:19:55 +0800

    add direct computation

--- Chapter2/Exercise2-1/range.c
+++ Chapter2/Exercise2-1/range.c
@@ -7,6 +7,8 @@
 #include<stdio.h>
 #include<limits.h>
 
+void computeLimit();
+
 main() {
     printf("Range of signed char is [%d, %d]\n", SCHAR_MIN, SCHAR_MAX);
     printf("Range of unsigned char is [%u, %u]\n", 0, UCHAR_MAX);
@@ -21,4 +23,22 @@ main() {
     printf("Range of signed long is [%ld, %ld]\n", LONG_MIN, LONG_MAX);
     printf("Range of unsigned long is [%d, %lu]\n", 0, ULONG_MAX);
 
+    computeLimit();
+
+}
+
+void computeLimit() {
+    printf("\n");
+    printf("Range of signed char is [%i, %i]\n", 0xffffff80, 0x7F);
+    printf("Range of unsigned char is [%u, %u]\n", 0, 0xFF);
+    printf("Range of char is [%d, %d]\n", 0xffffff80, 0x7f);
+
+    printf("Range of signed short is [%d, %d]\n", 0xffff8000, 0x7fff);
+    printf("Range of unsigned short is [%d, %d]\n", 0, 0xffff);
+
+    printf("Range of signed int is [%d, %d]\n", 0x80000000, 0x7fffffff);
+    printf("Range of unsigned int is [%u, %u]\n", 0, 0xffffffff);
+ 
+    printf("Range of signed long is [%ld, %ld]\n", 0x8000000000000000, 0x7fffffffffffffff);
+    printf("Range of unsigned long is [%d, %lu]\n", 0, 0xffffffffffffffff);
 }
