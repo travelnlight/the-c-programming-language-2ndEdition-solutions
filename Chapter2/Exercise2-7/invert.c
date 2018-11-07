5c1b1e86c958465083c07a36bdcc596edcef9750
travelnlight <na.tan11@outlook.com>
2018-11-07 10:36:24 +0800

    add mask comment

--- Chapter2/Exercise2-7/invert.c
+++ Chapter2/Exercise2-7/invert.c
@@ -1,6 +1,7 @@
 /**
  * Author: Na Tan <na.tan11@outlook.com>
  * Solution to Exercise 2-6.
+ *
  */
 
 #include<stdio.h>
@@ -12,6 +13,15 @@ unsigned invert(unsigned, int, int);
      printf("invert(105, 4, 3) %u\n", invert(105, 4, 3));
 
  }
+
+ /** Evolution of mask starting with ~0 shown in reverse order.
+  * 11100011
+  * 00011100
+  * 00000111
+  * 11111000
+  * 11111111
+  * 00000000
+  */
 
  unsigned invert(unsigned x, int p, int n) {
 
